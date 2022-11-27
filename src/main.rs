39f88aa87c1ef82d0e6e57e273525ff271b14ee9
MarcosA <mastermarcos1212@gmail.com>
2022-11-27 10:05:37 -0300

    Exercicio de tempo de vida.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod build_full_name;
 mod error_handler;
 mod factory;
+mod life_time;
 
 #[allow(dead_code)]
 fn tests() {
     factory::test();
     error_handler::test();
     build_full_name::test();
+    life_time::test();
 }
 
 fn main() {
